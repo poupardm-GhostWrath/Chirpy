b395e269222ef9a722a81cebe78039ab7a4863d3
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-04-16 09:51:33 -0600

    Get All Chirps Lesson

--- handler_chirps.go
+++ handler_chirps.go
@@ -85,4 +85,24 @@ func getCleanedBody(body string, badWords map[string]struct{}) string {
 	}
 	cleanedWords := strings.Join(words, " ")
 	return cleanedWords
-} 
+}
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.db.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+		return
+	}
+
+	var chirps []Chirp
+	for _, dbChirp := range dbChirps {
+		chirps = append(chirps, Chirp{
+			ID: dbChirp.ID,
+			CreatedAt: dbChirp.CreatedAt,
+			UpdatedAt: dbChirp.UpdatedAt,
+			Body: dbChirp.Body,
+			UserID: dbChirp.UserID,
+		})
+	}
+	respondWithJSON(w, http.StatusOK, chirps)
+}
